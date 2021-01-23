42a1552bafa9c124728b7a4341117150eabd6859
Xeoth <46075322+Xeoth@users.noreply.github.com>
2021-01-23 23:38:47 +0100

    added submission fetching

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
+import Submission from "./models/Submission.ts";
+import { FullnamePrefixes } from "./utils/constants.ts";
 import Requests, { RedditOptions } from "./utils/requests.ts";
 
 export default class Reddit {
+  req: Requests;
   constructor(options: RedditOptions) {
+    this.req = new Requests(options);
+  }
+
+  submission(id: string) {
+    return new Submission(FullnamePrefixes.Submission + id, this.req);
   }
 }
