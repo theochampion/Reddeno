d70efebe7a3720b2a4f49346a6741835e98a6772
Xeoth <46075322+Xeoth@users.noreply.github.com>
2021-03-02 09:46:52 +0100

    Some more tests

--- test/submission.test.ts
+++ test/submission.test.ts
@@ -1,11 +1,12 @@
 import Reddit from "../src/index.ts";
 import {
   assertEquals,
+  assertMatch,
   assertStrictEquals,
 } from "https://deno.land/std@0.88.0/testing/asserts.ts";
 
 Deno.test({
-  name: "isCorrectAuthor",
+  name: "correctSubmissionValues",
   fn: async function (): Promise<void> {
     const reddit = new Reddit({
       clientID: Deno.env.get("REDDIT_CLIENT_ID")!,
@@ -20,6 +21,17 @@ Deno.test({
     assertStrictEquals(
       submission.title,
       "Changes to Reddit’s Political Ads Policy",
+    );
+
+    assertStrictEquals(submission.subreddit, "announcements");
+    assertEquals(submission.creationDate, new Date(1586842538000));
+    assertStrictEquals(
+      submission.url,
+      "https://www.reddit.com/r/announcements/comments/g0s6tn/changes_to_reddits_political_ads_policy/",
+    );
+    assertMatch(
+      submission.text!,
+      /^As the 2020 election approaches, we are updating our/,
     );
   },
   sanitizeOps: false,
